45641519644665221f882994299add2b19fa26f1
Brandon Poe <Bpoe@users.noreply.github.com>
2024-04-02 17:26:11 -0700

    using desiredState object

---
+++ src/WhatIf.Model/Deployment.cs
@@ -0,0 +1,23 @@
+﻿namespace WhatIf.Model;
+
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+[DataContract]
+public class Deployment
+{
+    /// <summary>
+    /// The location to store the deployment data.
+    /// </summary>
+    /// <value>The location to store the deployment data.</value>
+    [DataMember(Name = "location", EmitDefaultValue = false)]
+    [JsonProperty(PropertyName = "location")]
+    public string Location { get; set; }
+
+    /// <summary>
+    /// Gets or Sets Properties
+    /// </summary>
+    [DataMember(Name = "properties", EmitDefaultValue = false)]
+    [JsonProperty(PropertyName = "properties")]
+    public DeploymentProperties Properties { get; set; }
+}
